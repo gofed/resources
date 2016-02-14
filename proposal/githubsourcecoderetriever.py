86b8390d79dc96fbe8885108dbd749487313c811
Jan Chaloupka <jchaloup@redhat.com>
2016-02-14 20:04:48 +0100

    Move remote resource retrieval to Retriever

--- proposal/githubsourcecoderetriever.py
+++ proposal/githubsourcecoderetriever.py
@@ -1,7 +1,4 @@
 from retriever import Retriever
-import urllib2
-import tempfile
-from ..exceptions import ResourceInvalidResourceError
 
 class GithubSourceCodeRetriever(Retriever):
 
@@ -20,29 +17,7 @@ class GithubSourceCodeRetriever(Retriever):
 		# https://REPOSITORY_URL/archive/COMMIT/name.tar.gz
 		tarball_url = "https://%s/archive/%s/name.tar.gz" % (repository_url, commit)
 
-		# TODO(jchaloup): catch exceptions: urllib2.URLError, urllib2.HTTPError
-		#	raise ResourceNotRetrieved instead?
-		try:
-			response = urllib2.urlopen(tarball_url)
-		except urllib2.URLError as err:
-			# can a user do something about it?
-			msg = "Unable to retrieve resource, url = %s, err = " % (tarball_url, err)
-			raise urllib2.URLError(msg)
-		except urllib2.HTTPError as err:
-			# can a user do something about it?
-			msg = "Unable to retrieve resource, url = %s, err = " % (tarball_url, err)
-			raise urllib2.HTTPError(msg)
-
-		try:
-			with tempfile.NamedTemporaryFile(delete=False) as f:
-				f.write(response.read())
-				f.flush()
-		except IOError as e:
-			# can a user do something about it?
-			msg = "Unable to store retrieved resource, err = " % (err)
-			raise ResourceUnableToRetrieveError(msg)
-
-		return f.name
+		return self._retrieveResource(tarball_url)
 
 if __name__ == "__main__":
 	GithubSourceCodeRetriever().retrieve("github.com/coreos/etcd", "d1b5fdea252dd5e4cafd2e7ccd7c900c2844bbe8")
--- proposal/retriever.py
+++ proposal/retriever.py
@@ -1,4 +1,34 @@
+import urllib2
+import tempfile
+from ..exceptions import ResourceUnableToRetrieveError
+
 class Retriever(object):
 
 	def retrieve(self, *args):
 		raise NotImplementedError()
+
+	def _retrieveResource(self, resource_url):
+		# TODO(jchaloup): catch exceptions: urllib2.URLError, urllib2.HTTPError
+		#	raise ResourceNotRetrieved instead?
+		try:
+			response = urllib2.urlopen(resource_url)
+		except urllib2.URLError as err:
+			# can a user do something about it?
+			msg = "Unable to retrieve resource, url = %s, err = " % (resource_url, err)
+			raise urllib2.URLError(msg)
+		except urllib2.HTTPError as err:
+			# can a user do something about it?
+			msg = "Unable to retrieve resource, url = %s, err = " % (resource_url, err)
+			raise urllib2.HTTPError(msg)
+
+		try:
+			with tempfile.NamedTemporaryFile(delete=False) as f:
+				f.write(response.read())
+				f.flush()
+		except IOError as e:
+			# can a user do something about it?
+			msg = "Unable to store retrieved resource, err = " % (err)
+			raise ResourceUnableToRetrieveError(msg)
+
+		return f.name
+
