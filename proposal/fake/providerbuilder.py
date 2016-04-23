69c210052e694a3bd073c378cf6101937bdde2ed
Jan Chaloupka <jchaloup@redhat.com>
2016-04-23 14:02:08 +0200

    Introduce FakeProviderBuilder

---
+++ proposal/fake/providerbuilder.py
@@ -0,0 +1,29 @@
+from ..providerbuilder import ProviderBuilder
+import os
+
+class FakeProviderBuilder(object):
+
+	def __init__(self):
+		pass
+
+	def buildGithubSourceCodeProvider(self):
+		return self
+
+	def buildRpmProvider(self):
+		return self
+
+	def buildGitRepositoryProvider(self):
+		return self
+
+	def buildMercurialRepositoryProvider(self):
+		return self
+
+	def __getattr__(self, name):
+		if name == "provide":
+			return self.provide
+
+		raise AttributeError("Method '%s' not found" % name)
+
+	def provide(self, *args):
+		return "resource/location"
+
