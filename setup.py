2db8bcd939c7e0dd8697d56722dc6faf08c7a56d
Jan Chaloupka <jchaloup@redhat.com>
2016-09-17 00:32:54 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gofedresources',
+    version='0.0.1',
+    description='Gofed resources',
+    long_description=''.join(open('README.md').readlines()),
+    keywords='gofed,resources',
+    author='Jan Chaloupka',
+    author_email='jchaloup@redhat.com',
+    url='https://github.com/gofed/resources',
+    license='GPL',
+    packages=['gofedresources', 'gofedresources.config'],
+    install_requires=open('requirements.txt').read().splitlines()
+)
